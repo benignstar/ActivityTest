800070d136d9e5cdb84266b1df63bd9ff927db76
benignstar <che0009@naver.com>
2017-04-13 20:51:48 +0900

    사용자가 입력한 값을 SecondActivity에 전달하기

--- app/src/main/java/benignstar/kr/hs/emirim/activitytest/SecondActivity.java
+++ app/src/main/java/benignstar/kr/hs/emirim/activitytest/SecondActivity.java
@@ -1,19 +1,26 @@
 package benignstar.kr.hs.emirim.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    String name, tel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+        TextView result=(TextView)findViewById(R.id.text_result);
         Button butSecond=(Button)findViewById(R.id.but_second);
         butSecond.setOnClickListener(butSecondHandler);
+
+        Intent intent = getIntent();
+        name=intent.getStringExtra("name");
+        tel=intent.getStringExtra("tel");
+        result.setText("성명 : "+name+"\n전화번호 : "+tel);
     }
 
     View.OnClickListener butSecondHandler= new View.OnClickListener() {
